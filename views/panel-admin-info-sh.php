fae7a759bdf793e6145a09aab75ae0cd3c7d0779
davidvalbuenae <74325886+davidvalbuenae@users.noreply.github.com>
2022-05-03 18:41:17 -0500

    Update info with ids

--- views/panel-admin-info-sh.php
+++ views/panel-admin-info-sh.php
@@ -17,11 +17,11 @@
         </div>
         <div class="info">
             <h3>¿Cómo agregar un super heroe a un POST?</h3>
-            <p>Agregar el siguiente shortcode [heroe id="1"] con el id del heroe que quieres tener en el post o pagina. (Revisar los ID en la lista de heroes que está en la parte superior vea la tabla.)</p>
+            <p>Agregar el siguiente shortcode [heroe ids="644"] con el id del heroe que quieres tener en el post o pagina. (Revisar los ID en la lista de heroes que está en la parte superior vea la tabla.)</p>
         </div>
         <div class="info">
             <h3>¿Cómo agregar varios super heroes a un POST?</h3>
-            <p>Agregar el siguiente shortcode [heroe id="1, 5, 4, 3"] con los id de los heroes que quieres tener en el post o pagina. (Revisar los ID en la lista de heroes que está en la parte superior vea la tabla.)</p>
+            <p>Agregar el siguiente shortcode [heroe ids="644, 620, 332, 659"] con los id de los heroes que quieres tener en el post o pagina. (Revisar los ID en la lista de heroes que está en la parte superior vea la tabla.)</p>
         </div>
         <div class="info">
             <h3>¿Cómo se ve un super heroe en algun POST?</h3>
